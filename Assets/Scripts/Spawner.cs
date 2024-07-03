b31de154c6ae934aeb1b886773006f55e5b97ae6
FPGLord <127301692+FPGLord@users.noreply.github.com>
2024-07-03 19:42:53 +0300

    Jump, scene transition

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,122 +1,63 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private GameObject[] _gameObjects;
+    [SerializeField] private Objects[] _gameObjects;
+    [SerializeField] private GameObject _viewPrefab;
+   
+    private List<GameObject> _spawnViews = new();
+    private int _index;
 
-    private GameObject[] _objects;
-    private List<int> _availableSpawnIndex;
 
     private void Start()
     {
-        _objects = new GameObject[_gameObjects.Length];
-        _availableSpawnIndex = new List<int>();
-        for (int i = 0; i < _spawnPoints.Length; i++)
-        {
-            _availableSpawnIndex.Add(i); 
-        }
-
-        InstantializeObjects();
+        Spawn();
+        ShuffleSpawnPoints();
         StartCoroutine(SpawnCoroutine());
     }
 
-    private void InstantializeObjects()
+    public void Spawn()
     {
+        foreach (var item in _spawnViews)
+        {
+            item.SetActive(true);
+        }
+
         for (int i = 0; i < _gameObjects.Length; i++)
         {
-            if (_availableSpawnIndex.Count > 0)
+            if (_spawnViews.Count <=1)
             {
-                int spawnIndex = _availableSpawnIndex[Random.Range(0, _availableSpawnIndex.Count)];
-                _objects[i] = Instantiate(_gameObjects[i], _spawnPoints[spawnIndex].position, Quaternion.identity);
-                _availableSpawnIndex.Remove(spawnIndex); 
+                GameObject newView = Instantiate(_viewPrefab, transform);
+                _spawnViews.Add(newView);
             }
         }
+        
     }
 
-    public void RespawnObjects()
+    
+    
+    private void ShuffleSpawnPoints()
     {
         for (int i = 0; i < _spawnPoints.Length; i++)
         {
-            if (!_objects.Any(obj => obj.transform.position == _spawnPoints[i].position))
-            {
-                _availableSpawnIndex.Add(i);
-            }
+            int randomObstacleIndex = Random.Range(0, _spawnPoints.Length);
+            (_spawnPoints[i], _spawnPoints[randomObstacleIndex]) =
+                (_spawnPoints[randomObstacleIndex], _spawnPoints[i]);
         }
-
-        InstantializeObjects(); 
     }
 
     IEnumerator SpawnCoroutine()
     {
-        while (true)
+        while (_index ==7)
         {
+            Spawn();
             yield return new WaitForSeconds(3f);
-            RespawnObjects();
+            _index++;
         }
     }
-
-
-    // [SerializeField] private Transform[] _spawnPoints;
-    // [SerializeField] private GameObject[] _gameObjects;
-    //
-    //
-    // private GameObject[] _objects;
-    //
-    // private void Start()
-    // {
-    //     Instantiate();
-    //     ShuffleSpawnPoints();
-    //     StartCoroutine(SpawnCoroutine());
-    // }
-    //
-    //
-    // private void SpawnObjects()
-    // {
-    //     for (int i = 0; i < _objects.Length; i++)
-    //     {
-    //         _objects[i].transform.position = _spawnPoints[i].position;
-    //     }
-    // }
-    //
-    // private void Instantiate()
-    // {
-    //     _objects = new GameObject[_gameObjects.Length];
-    //     for (int i = 0; i < _gameObjects.Length; i++)
-    //     {
-    //         _objects[i] = Instantiate(_gameObjects[i], transform);
-    //         
-    //     }
-    // }
-    //
-    // public void RespawnObjects()
-    // {
-    //     ShuffleSpawnPoints(); 
-    //     SpawnObjects();       
-    // }
-    //
-    // IEnumerator SpawnCoroutine()
-    // {
-    //     while (true)
-    //     {
-    //         yield return new WaitForSeconds(3f); 
-    //         RespawnObjects();                   
-    //     }
-    // }
-    //
-    //
-    // private void ShuffleSpawnPoints()
-    // {
-    //     for (int i = 0; i < _spawnPoints.Length; i++)
-    //     {
-    //         int randomObstacleIndex = Random.Range(0, _spawnPoints.Length);
-    //         (_spawnPoints[i], _spawnPoints[randomObstacleIndex]) =
-    //             (_spawnPoints[randomObstacleIndex], _spawnPoints[i]);
-    //     }
-    // }
+   
 }
